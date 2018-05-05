947488e3af97d83dcd34268481c3180dbbbd78ce
MikahJC <mikah.chapman@gmail.com>
2018-05-05 12:53:43 -0600

    initial index rewrite

--- web/index.php
+++ web/index.php
@@ -1,5 +1,10 @@
-<?php
-
-  phpinfo();
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Mikah's CS 313 Homepage</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<img class="center" src="gbc.png">
+</body>
+</html>
