147ec38080c3f96bdd9107b05f502200292fdb3c
MikahJC <mikah.chapman@gmail.com>
2018-05-05 17:53:56 -0600

    test menu position

--- web/index.php
+++ web/index.php
@@ -7,6 +7,10 @@
 <body>
     <div id="gbc" class="container">
         <img class="center" src="gbc.png" width="540" height="902">
+        <div class="menuItems">
+            <p>Week 1</p>
+            <p>Week 2</p>
+        </div>
     </div>
 </body>
 </html>
