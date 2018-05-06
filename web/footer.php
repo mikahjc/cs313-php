b4d1dd9b85f9aec1e3fad66da5d58e18f2248072
MikahJC <mikah.chapman@gmail.com>
2018-05-05 21:38:44 -0600

    typo

--- web/footer.php
+++ web/footer.php
@@ -1,4 +1,4 @@
 <footer>
     <p><a href="https://decatilde.deviantart.com/art/Game-Boy-Color-skins-for-VisualBoyAdvance-303673863">Gameboy Color skin by decatilde</a></p>
-    <p>Game Boy, Game Boy Color, Pokemon and all repective names are Trademark & &copy; of Nintendo 1989-<?php echo date("Y");?></p>
+    <p>Game Boy, Game Boy Color, Pokemon and all respective names are Trademark & &copy; of Nintendo 1989-<?php echo date("Y");?></p>
 </footer>
