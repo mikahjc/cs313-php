aa60e34def35d8724e4d3192057ef6b59828720d
MikahJC <mikah.chapman@gmail.com>
2018-05-05 19:42:04 -0600

    finish time display

--- web/index.php
+++ web/index.php
@@ -9,7 +9,15 @@
         <img class="center" src="gbc_main.png" width="540" height="902" usemap="#screenMap">
         <div class="timeDisplay">
             <?php
-                echo strtoupper(date("l"))."<br>   TIME";
+                echo strtoupper(date("l"))."<br>".str_repeat('&nbsp;',3);
+                if (date("G") < 4 || date("G") > 17) {
+                    echo "NITE&nbsp;";
+                } elseif (date("G") > 4 || date("G") < 10) {
+                    echo "MORN&nbsp;";
+                } else {
+                    echo "DAY&nbsp;";
+                }
+                echo date("g:i");
             ?>
         </div>
     </div>
