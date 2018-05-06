97384bf2a1422f84e9560c2c6195b1601e952dbe
MikahJC <mikah.chapman@gmail.com>
2018-05-05 19:22:45 -0600

    add homepage

--- web/index.php
+++ web/index.php
@@ -1,20 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Mikah's CS 313 Homepage</title>
-<link rel="stylesheet" href="style.css">
+    <title>Mikah's CS 313 Homepage</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div id="gbc" class="container">
-        <img class="center" src="gbc.png" width="540" height="902">
-        <div class="menuItems">
-            <p><a class="menuEntry" href="">ABOUT ME</a></p>
-            <p><a class="menuEntry" href="">WEEK 2</a></p>
-            <p><a class="menuEntry" href="">WEEK 3</a></p>
-            <p><a class="menuEntry" href="">WEEK 4</a></p>
-            <p><a class="menuEntry" href="">WEEK 5</a></p>
-        </div>
-        <div class="dialog">Choose a page.</div>
+        <img class="center" src="gbc_main.png" width="540" height="902" usemap="#screenMap">
     </div>
+    <map name="screenMap">
+        <area shape="rect" coords="123,105,367,137" href="selection.php" alt="Continue">
+    </map>
 </body>
 </html>
---
+++ web/selection.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Mikah's CS 313 Selection</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="gbc" class="container">
+        <img class="center" src="gbc_select.png" width="540" height="902">
+        <div class="menuItems">
+            <p><a class="menuEntry" href="index.php">ABOUT ME</a></p>
+            <p><a class="menuEntry" href="">WEEK 3</a></p>
+            <p><a class="menuEntry" href="">WEEK 4</a></p>
+            <p><a class="menuEntry" href="">WEEK 5</a></p>
+        </div>
+        <div class="dialog">Choose a page.</div>
+    </div>
+</body>
+</html>
