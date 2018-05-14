a5d535af08719e85eb105e7e1483bdaf80134dcd
Mikah Chapman <mikah.chapman@gmail.com>
2018-05-13 20:47:23 -0600

    fixed order submission sprites

--- web/week3/submit.php
+++ web/week3/submit.php
@@ -33,7 +33,7 @@
 					foreach($_SESSION['cart'] as $item => $quantity ) {
 						echo "<div class='item'>";
 						echo "<span class='title'>".$itemNames[$item]."</span>";
-						echo "<img src='".$spritepath.$itemSprites[$item]."'>";
+						echo "<img src='".$spritepath.$itemSprites[$item].".png'>";
 						echo "<div><div></div><div class='add'>".$quantity." @ $";
 						printf("%.2f",$itemPrices[$item]);
 						echo "</div></div>";
