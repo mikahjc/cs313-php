7964c09da52b6c9cad7b26513d8645640f3427ae
mikahjc <mikah.chapman@gmail.com>
2018-05-25 23:11:27 -0700

    strategydex working + sprites

--- web/footer.php
+++ web/footer.php
@@ -2,6 +2,11 @@
 	<p>
 	<?php if (isset($GBC_NOTICE)) {
 		echo "<a href='https://decatilde.deviantart.com/art/Game-Boy-Color-skins-for-VisualBoyAdvance-303673863'>Gameboy Color skin by decatilde</a></p><p>Game Boy, Game Boy Color, ";
-	} ?>
-    Pokemon and all respective names are Trademark & &copy; of Nintendo 1989-<?php echo date("Y");?></p>
+	}
+	if (strpos($_SERVER['REQUEST_URI'], 'teambuilder') !== false) {
+		echo "Pokemon sprites were retrieved from the <a href='https://www.smogon.com/dex/sm/pokemon/'>Smogon Sun/Moon Strategy Pokedex</a>.</p>
+		<p>Data from the Pokemon games (moves, items, abilities, Pokemon) were retrieved from <a href='https://github.com/Zarel/Pokemon-Showdown'/>Pokemon Showdown</a> source code.</p><p>";
+	}
+	?>
+    Pokemon and all respective names are Trademark & &copy; of Nintendo 1989-<?=date("Y")?>.</p>
 </footer>
--- web/teambuilder/strategydex.php
+++ web/teambuilder/strategydex.php
@@ -19,7 +19,7 @@
     }
     
     // Get types
-    $stmt = $db->prepare('SELECT id, name, gen FROM pokemon;');
+    $stmt = $db->prepare('SELECT id, name, gen FROM pokemon ORDER BY id;');
     $stmt->execute();
     $pokemonList = $stmt->fetchAll(PDO::FETCH_ASSOC);
     
@@ -28,7 +28,7 @@
         if ($pokemonId != "") {
             $longStatement =  "SELECT pv.name, pv.number, pv.type1, pv.type2, pv.previous_evolution, pv.base_hp, pv.base_attack, pv.base_defense, pv.base_special_attack, pv.base_special_defense, pv.base_speed\n";
             $longStatement .= "FROM pokemon_view pv\n";
-            $longStatement .= "WHERE pv.id=:pokemonId;";
+            $longStatement .= "WHERE pv.id=:pokemonId";
             $stmt = $db->prepare($longStatement);
             $stmt->bindValue("pokemonId", $pokemonId, PDO::PARAM_INT);
             $stmt->execute();
@@ -39,7 +39,7 @@
     <h1>Mini StrategyDex</h1>
     <h2>Select Pokemon:</h2>
     <form action="strategydex.php" type='GET'>
-    Type: <select name="typeFilter">
+    Type: <select name="pokemon">
         <option></option>
         <?php foreach($pokemonList as $row) {
             $id = $row["id"];
@@ -56,39 +56,50 @@
 </form><br>
 <?php
 if (isset($pokemonData)) { 
-echo "<h3>
-<table>
-<tr>
-    <th></th>
-    <th>Pokedex #</th>
-    <th>Type 1</th>
-    <th>Type 2</th>
-    <th>Evolves from</th>
-    <th>HP</th>
-    <th>Atk.</th>
-    <th>Def.</th>
-    <th>Sp. Atk.</th>
-    <th>Sp. Def.</th>
-    <th>Speed</th>
-</tr>";
-    $number = $row["number"];
-    $name = $row["name"];
-    $type1 = $row["type1"];
-    $type2 = $row["type2"];
-    $evolution = $row["previous_evolution"];
-    $hp = $row["base_hp"];
-    $atk = $row["base_attack"];
-    $def = $row["base_defense"];
-    $spatk = $row["base_special_attack"];
-    $spdef = $row["base_special_defense"];
-    $speed = $row["base_speed"];
-    echo "<tr><td><img src='https://www.smogon.com/dex/media/sprites/xy/$name.gif/></td>";
-    if ($power == 0) {
-        echo "<td>--</td>";
-    } else {
-        echo "<td>$power</td>";
+$pokemonData = $pokemonData[0];
+$number = $pokemonData["number"];
+    $name = $pokemonData["name"];
+    $type1 = $pokemonData["type1"];
+    $type2 = $pokemonData["type2"];
+    $evolution = $pokemonData["previous_evolution"];
+    $hp = $pokemonData["base_hp"];
+    $atk = $pokemonData["base_attack"];
+    $def = $pokemonData["base_defense"];
+    $spatk = $pokemonData["base_special_attack"];
+    $spdef = $pokemonData["base_special_defense"];
+    $speed = $pokemonData["base_speed"];
+
+    if ($evolution == "") {
+        $evolution = "Nothing";
     }
-    echo "<td>$accuracy</td><td>$pp</td><td>$description</td></tr>\n";
+echo "<h2>$name</h2>
+<table>
+    <tr>
+        <th></th>
+        <th>Pokedex #</th>
+        <th>Type 1</th>
+        <th>Type 2</th>
+        <th>Evolves from</th>
+        <th>HP</th>
+        <th>Atk.</th>
+        <th>Def.</th>
+        <th>Sp. Atk.</th>
+        <th>Sp. Def.</th>
+        <th>Speed</th>
+    </tr>
+    <tr>
+        <td><img src='/assets/img/sprites/pkmn/". strtolower($name) .".gif'/></td>
+        <td>$number</td>
+        <td>$type1</td>
+        <td>$type2</td>
+        <td>$evolution</td>
+        <td>$hp</td>
+        <td>$atk</td>
+        <td>$def</td>
+        <td>$spatk</td>
+        <td>$spdef</td>
+        <td>$speed</td>
+    </tr>";
      
 }
 ?>
